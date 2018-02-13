02bc17c8340ccfa3a91d23a131c7de4492da4da3
Karl Burkhardt <karlaburkhardt@gmail.com>
2018-02-12 20:34:10 -0500

    Add files via upload
    Python function to read temperature and relative humidity from Si7021.
    
    Syntax: humidity, temperature = Si7021()

---
+++ Si7021.py
@@ -0,0 +1,30 @@
+import smbus
+import time
+
+def Si7021():
+
+    bus = smbus.SMBus(1) # I2C bus
+    address = 0x40 # Si7021 address
+    hum = 0xF5 # measure relative humidity command
+    temp = 0xE0 # read temperature from humidity command
+    # NB: temperature is read with humidity to get relative
+    # humidity, temperature need not be measured again
+    
+    bus.write_byte(address, hum) # measure hum and temp
+    time.sleep(0.1)
+
+    msbyte = bus.read_byte(address)
+    lsbyte = bus.read_byte(address)
+
+    humidity = ((msbyte*256+lsbyte)*125/65536)-6
+
+    bus.write_byte(address, temp) # recalls temp
+    time.sleep(0.1)
+
+    msbyte = bus.read_byte(address)
+    lsbyte = bus.read_byte(address)
+
+    cTemp = ((msbyte*256+lsbyte)*175.72/65536)-46.85
+    fTemp = cTemp*1.8+32
+
+    return humidity, fTemp
